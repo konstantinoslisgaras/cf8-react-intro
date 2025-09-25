2106067a5e164ad7bcd70bb38b0f03929c08992c
konstantinoslisgaras <konstantinoslisgaras@gmail.com>
2025-09-25 20:48:05 +0300

    feat: move vite landing int a new component

---
+++ src/components/ArrowFunctionalComponent.tsx
@@ -0,0 +1,10 @@
+const ArrowFunctionalComponent =() => {
+    const title = "Is an Arrow Functional Component";
+    return (
+        <>
+            <div className={"text-center p-10 border-l-black"} ></div>
+            <h1 className="text-yellow-600 text-3xl font-medium mt-12">{title}<span>Test Span</span></h1>
+        </>)
+}
+
+export default ArrowFunctionalComponent;
---
+++ src/components/FunctionalComponent.tsx
@@ -0,0 +1,6 @@
+function FunctionalComponent() {
+    const title = "Is a Functional Component";
+    return <h1 className="text-center text-2xl font-bold mt-12 text-red-400">{title}</h1>
+}
+
+export default FunctionalComponent;
