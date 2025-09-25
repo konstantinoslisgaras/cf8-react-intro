116580f68bac93d48bf68afa4ff61e2a4b13831f
konstantinoslisgaras <konstantinoslisgaras@gmail.com>
2025-09-25 20:51:30 +0300

    feat: add different types of Components

--- src/components/ArrowFunctionalComponent.tsx
+++ src/components/ArrowFunctionalComponent.tsx
@@ -2,7 +2,7 @@ const ArrowFunctionalComponent =() => {
     const title = "Is an Arrow Functional Component";
     return (
         <>
-            <div className={"text-center p-10 border-l-black"} ></div>
+            <div className={"text-center p-10 border-r-amber-500"} ></div>
             <h1 className="text-yellow-600 text-3xl font-medium mt-12">{title}<span>Test Span</span></h1>
         </>)
 }
