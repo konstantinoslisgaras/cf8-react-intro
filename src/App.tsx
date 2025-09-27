6bf94430934bc418440af63f8f972a978350adb6
konstantinoslisgaras <konstantinoslisgaras@gmail.com>
2025-09-27 13:27:17 +0300

    feat: add different types of Components

--- src/App.tsx
+++ src/App.tsx
@@ -6,27 +6,34 @@ import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx"
 import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
 import ArrowFunctionalComponentWithPropsType from "./components/ArrowFunctionalComponentWithPropsType.tsx";
 import Card from "./components/Card.tsx";
+import Layout from "./components/Layout.tsx";
 
 function App() {
 
   return (
     <>
     {/*<ViteIntro />*/}
-        <ClassComponent />
-        <FunctionalComponent />
-        <ArrowFunctionalComponent />
-        <ArrowFunctionalComponentWithProps title="Is An Arrow Functional Component with Props" />
-        <ArrowFunctionalComponentWithProps />
-        <ArrowFunctionalComponentWithProps title="Second Arrow Functional Component with Props" />
-        <ArrowFunctionalComponentWithPropsType
-            title="Third Arrow Functional Component with Props"
-            description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus, similique!"/>
-        <Card title="Is a Card Functional Component">
+    {/*    <ClassComponent />*/}
+    {/*    <FunctionalComponent />*/}
+    {/*    <ArrowFunctionalComponent />*/}
+    {/*    <ArrowFunctionalComponentWithProps title="Is An Arrow Functional Component with Props" />*/}
+    {/*    <ArrowFunctionalComponentWithProps />*/}
+    {/*    <ArrowFunctionalComponentWithProps title="Second Arrow Functional Component with Props" />*/}
+    {/*    <ArrowFunctionalComponentWithPropsType*/}
+    {/*        title="Third Arrow Functional Component with Props"*/}
+    {/*        description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus, similique!"/>*/}
+    {/*    <Card title="Is a Card Functional Component">*/}
+    {/*        <ArrowFunctionalComponentWithPropsType*/}
+    {/*        title="Third Arrow Functional Component with Props"*/}
+    {/*        description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus, similique!"*/}
+    {/*        />*/}
+    {/*    </Card>*/}
+
+        <Layout>
             <ArrowFunctionalComponentWithPropsType
-            title="Third Arrow Functional Component with Props"
-            description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus, similique!"
-            />
-        </Card>
+                    title="Third Arrow Functional Component with Props"
+                    description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus, similique!"/>
+        </Layout>
     </>
   )
 }
---
+++ src/components/Layout.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import Header from "./Header.tsx";
+import Footer from "./Footer.tsx";
+
+interface LayoutProps {
+    children: React.ReactNode;
+}
+
+const Layout =({children}: LayoutProps) => {
+    return (
+        <>
+            <Header />
+                <div className="container mx-auto pt-24 min-h-[90vh]"></div>
+                {children}
+            <Footer />
+        </>
+    )
+}
+
+export default Layout;
