1af1d13fd8e4f33a5bdd15a3e58f35522559ea23
konstantinoslisgaras <konstantinoslisgaras@gmail.com>
2025-10-02 17:41:08 +0300

    fix: styling

--- src/components/Multiplier.tsx
+++ src/components/Multiplier.tsx
@@ -4,20 +4,15 @@ import MultiplierButton from "../components/MultiplierButton";
 const Multiplier = () => {
     const [multiplier, setMultiplier] = React.useState(1);
 
-    const IncreaseMult = () => {
-        setMultiplier(multiplier * 2);
-    };
-    const DecreaseMult = () => {
-        setMultiplier(multiplier / 2);
-    };
-    const ResetCount = () => {
-        setMultiplier(1);
-    };
+    const IncreaseMult = () => setMultiplier(multiplier * 2);   // Multiplies by 2
+    const DecreaseMult = () => setMultiplier(multiplier / 2);   // Divides by 2
+    const ResetCount = () => setMultiplier(1);  // Resets counter
+
     return (
         <>
             <div className="mt-10 flex flex-col justify-center items-center w-1/3 mx-auto rounded-md shadow-xl">
-                <h1 className="m-2 p-2 text-2xl">Multiplying and Dividing by 2</h1>
-                <h2 className="text-center font-mono m-2 p-2 border-2 rounded-sm inline-block">Number: {multiplier}</h2>
+                <h1 className="m-2 p-2 text-2xl">Binary Multiplier</h1>
+                <h2 className="text-center font-mono m-2 p-2 border-1 rounded-sm inline-block">Result: {multiplier}</h2>
                 <div className="text-center inline-flex items-center justify-center">
                     <MultiplierButton onClick={IncreaseMult}
                                       label="Multiply by 2"/>
